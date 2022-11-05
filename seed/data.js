5645e45aad10f2d0f8a553c74b113dd3da7dbf2b
Suji Falcon <suji.falcon@gmail.com>
2022-11-05 13:49:50 -0400

    seedable database for all champs

--- seed/data.js
+++ seed/data.js
@@ -1,52 +1,62 @@
 import axios from 'axios';
 import db from '../db/connection.js';
 import Character from '../models/Character.js';
-// import characters from './characters.json' assert { type: 'json' };
+
+const extractDataWeWant = (characters) => {
+	return characters.map((characterObj) => {
+		const charName = characterObj.id;
+		return {
+			name: charName,
+			title: characterObj.title,
+			blurb: characterObj.blurb,
+			lore: characterObj.lore,
+			tags: characterObj.tags,
+			partype: characterObj.partype,
+			passive: {
+				name: characterObj.name,
+				description: characterObj.description,
+				passive_image: `http://ddragon.leagueoflegends.com/cdn/img/champion/passive/${charName}_P.png`,
+			},
+			// For each spell, extract an object of data we want
+			spells: characterObj.spells.map((spell) => ({
+				id: spell.id,
+				name: spell.name,
+				description: spell.description,
+				spell_image: [
+					`http://ddragon.leagueoflegends.com/cdn/12.21.1/img/spell/${spell.id}.png`,
+				],
+			})),
+			image_loading: `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${charName}_0.jpg`,
+			image_splash: `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${charName}_0.jpg`,
+			image_square: `http://ddragon.leagueoflegends.com/cdn/img/champion/${charName}.png`,
+		};
+	});
+};
 
 const insertData = async (characters) => {
-	const charactersData = [];
-
-	for (const char in characters) {
-		// let spells = characters[char].spells.id;
-
-		charactersData.push({
-			name: char,
-			title: characters[char].title,
-			blurb: characters[char].blurb,
-			lore: characters[char].lore,
-			tags: characters[char].tags,
-			partype: characters[char].partype,
-			passive: {
-				name: characters[char].name,
-				description: characters[char].description,
-				passive_image: `http://ddragon.leagueoflegends.com/cdn/img/champion/passive/${char}_P.png`,
-			},
-			spells: [
-				{
-					id: characters[char].id,
-					name: characters[char].name,
-					description: characters[char].description,
-					spell_image: [
-						`http://ddragon.leagueoflegends.com/cdn/12.21.1/img/spell/${spells}.png`, //iterate thru for each champs spell, 4 of them
-					],
-				},
-			],
-			image_loading: `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${char}_0.jpg`,
-			image_splash: `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${char}_0.jpg`,
-			image_square: `http://ddragon.leagueoflegends.com/cdn/img/champion/${char}.png`,
-		});
-	}
-
-	// reset database
 	await db.dropDatabase();
-	// insert data
-	await Character.create(charactersData);
-	// close db connection
+
+	const reqPromises = Object.keys(characters).map((char) => {
+		return axios.get(
+			`http://ddragon.leagueoflegends.com/cdn/12.21.1/data/en_US/champion/${char}.json`,
+		);
+	});
+
+	const characterData = await (
+		await Promise.all(reqPromises)
+	).map((char) => {
+		const champName = Object.keys(char.data.data)[0];
+		return char.data.data[champName];
+	});
+	// Now that we have *all* the data, extract just the data that we want
+	const dataWeWant = extractDataWeWant(characterData);
+
+	await Character.create(dataWeWant);
 	await db.close();
 };
 
 const fetchCharacters = async () => {
-	let characters = await axios.get(
+	const characters = await axios.get(
 		`http://ddragon.leagueoflegends.com/cdn/12.21.1/data/en_US/champion.json`,
 	);
 	insertData(characters.data.data);
